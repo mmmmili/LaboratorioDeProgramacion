6ad72c9fbefefd6ffa3d8a54d7935cb542823a3c
Agus <aemanuel.ferreyra@est.fi.uncoma.edu.ar>
2025-08-14 19:52:00 -0300

    clase hamburguesa default

--- TP1/Hamburguesa.java
+++ TP1/Hamburguesa.java
@@ -1,5 +1,19 @@
 package TP1;
 
 public class Hamburguesa {
-    
+    private double precio;
+    private String descripcion;
+
+
+    public Hamburguesa(){
+        precio =0;
+        descripcion= "una hamburguesa";
+
+    }
+
+    public void armarHamburguesa(){
+        precio = 10;
+        descripcion= "2 panes y 1 carne";
+
+    }
 }
