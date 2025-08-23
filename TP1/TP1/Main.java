c4c4d34c7ad60a27933892abac560ce1e99be9ba
Jeremias <jalenieckij@gmail.com>
2025-08-23 19:58:58 -0300

    arreglado main

--- TP1/TP1/Main.java
+++ TP1/TP1/Main.java
@@ -2,7 +2,7 @@ package TP1;
 
 public class Main {
     public static void main(String[] args) {
-        HamburguesaInterfaz hambur = new PancetaDecorator(new HuevoDecorator(new QuesoDecorator(new Hamburguesa())));
+        HamburguesaInterfaz hambur = new PancetaDecorator((new Hamburguesa()));
         //esta verga no anda dios sabra porque
         //link del video del que nos estabamos basando: 
         // https://www.youtube.com/watch?v=j40kRwSm4VE
