b3be1e54eef8cc19754d6f19e4cae02b6da8e7ff
Agus <aemanuel.ferreyra@est.fi.uncoma.edu.ar>
2025-10-09 18:26:26 -0300

    mapitas ready

--- TP2/script.js
+++ TP2/script.js
@@ -10,7 +10,6 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "07:00hs",
         contacto: "https://www.instagram.com/fiesta_kiwi/",
-        ubicacion: "https://maps.app.goo.gl/HFv7RjbetbU9fR1CA",
         url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
     },
     
@@ -21,7 +20,7 @@ const eventos = {
         apertura: "00:30hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/moodliveok",
-        ubicacion: "https://maps.app.goo.gl/HFLYTMRo8y1SW6pb8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.790159065663!2d-68.0583724!3d-38.951622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33196ae9bf47%3A0x8935eaa087a2d345!2sMood%20Live!5e0!3m2!1ses!2sar!4v1760044539504!5m2!1ses!2sar",
     },
     bresh: {
         titulo: "BRESH",
@@ -30,7 +29,7 @@ const eventos = {
         apertura: "00:30hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/bresh",
-        ubicacion: "https://maps.app.goo.gl/vNpyynu9TzPmdJPH7",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.674307862607!2d-68.1380886!3d-38.9542683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a333579b9b721%3A0xb25a8725a0874a24!2sEspacio%20Duam!5e0!3m2!1ses!2sar!4v1760044955380!5m2!1ses!2sar",
     },
     summer: {
         titulo: "Summer",
@@ -39,7 +38,7 @@ const eventos = {
         apertura: "00:30hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/summerfest",
-        ubicacion: "https://maps.app.goo.gl/vNpyynu9TzPmdJPH7",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.674307862607!2d-68.1380886!3d-38.9542683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a333579b9b721%3A0xb25a8725a0874a24!2sEspacio%20Duam!5e0!3m2!1ses!2sar!4v1760044955380!5m2!1ses!2sar",
     },
     tulum: {
         titulo: "Slay",
@@ -48,7 +47,7 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "07:00hs",
         contacto: "https://instagram.com/tulumbarok",
-        ubicacion: "https://maps.app.goo.gl/jsMpK9o7WoRUmy477",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.8704538165!2d-68.06197209999999!3d-38.949787799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a3300645c25d1%3A0x4879de12bb99adb0!2stulum!5e0!3m2!1ses!2sar!4v1760045029466!5m2!1ses!2sar",
     },
     /*------- TECNO -------- */
     tecno: {
@@ -58,7 +57,7 @@ const eventos = {
         apertura: "23:00hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/vision.festival",
-        ubicacion: "https://maps.app.goo.gl/aKwALfqynxykBW5s8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
 
     },
     tecno2: {
@@ -68,7 +67,7 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/vision.festival",
-        ubicacion: "https://maps.app.goo.gl/aKwALfqynxykBW5s8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
     },
     tecno3: {
         titulo: "Vision - Fideles + Lion + Farizano",
@@ -77,7 +76,7 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/vision.festival",
-        ubicacion: "https://maps.app.goo.gl/aKwALfqynxykBW5s8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
     },
     tecno4: {
         titulo: "Vision - Simon Vuarambon (Extended Set) + Doyel",
@@ -86,7 +85,7 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/vision.festival",
-        ubicacion: "https://maps.app.goo.gl/aKwALfqynxykBW5s8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
     },
     vision2: {
         titulo: "Primavera Vision",
@@ -95,7 +94,7 @@ const eventos = {
         apertura: "00:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/vision.festival",
-        ubicacion: "https://maps.app.goo.gl/aKwALfqynxykBW5s8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24821.156123862045!2d-68.08900098916011!3d-38.954948799999976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462a98bef%3A0xb9170f526e01c4d1!2sLA%20NONNINA%20Espacio%20de%20Eventos!5e0!3m2!1ses!2sar!4v1759432761341!5m2!1ses!2sar",
     },
     /*--------- RKT --------- */
     rkt: {
@@ -105,7 +104,7 @@ const eventos = {
         apertura: "01:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/rktfiesta",
-        ubicacion: "https://maps.app.goo.gl/HFv7RjbetbU9fR1CA",
+        url: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6205.27475851252!2d-68.0529558!3d-38.9551118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462cc3157%3A0x51e8005c84277f11!2sIndependencia%20428%2C%20Q8300%20Neuqu%C3%A9n!5e0!3m2!1ses!2sar!4v1760044851950!5m2!1ses!2sar",
     },
     caserio: {
         titulo: "El Caserío Club",
@@ -114,7 +113,7 @@ const eventos = {
         apertura: "01:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/elcaserioclub",
-        ubicacion: "https://maps.app.goo.gl/HFv7RjbetbU9fR1CA",
+        url: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6205.27475851252!2d-68.0529558!3d-38.9551118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462cc3157%3A0x51e8005c84277f11!2sIndependencia%20428%2C%20Q8300%20Neuqu%C3%A9n!5e0!3m2!1ses!2sar!4v1760044851950!5m2!1ses!2sar",
     },
     wasabi: {
         titulo: "Wasabi Neuquén",
@@ -123,7 +122,7 @@ const eventos = {
         apertura: "23:00hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/moodliveok",
-        ubicacion: "https://maps.app.goo.gl/HFLYTMRo8y1SW6pb8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.790159065663!2d-68.0583724!3d-38.951622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33196ae9bf47%3A0x8935eaa087a2d345!2sMood%20Live!5e0!3m2!1ses!2sar!4v1760044539504!5m2!1ses!2sar",
     },
     modoRkt: {
         titulo: "Modo RKT",
@@ -132,7 +131,7 @@ const eventos = {
         apertura: "23:00hs",
         cierre: "06:00hs",
         contacto: "https://instagram.com/modorkt",
-        ubicacion: "https://maps.app.goo.gl/HFv7RjbetbU9fR1CA",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.790159065663!2d-68.0583724!3d-38.951622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33196ae9bf47%3A0x8935eaa087a2d345!2sMood%20Live!5e0!3m2!1ses!2sar!4v1760044539504!5m2!1ses!2sar",
     },
     caserio2: {
         titulo: "El Caserío Club - DJs Invitados",
@@ -141,7 +140,7 @@ const eventos = {
         apertura: "01:00hs",
         cierre: "08:00hs",
         contacto: "https://instagram.com/elcaserioclub",
-        ubicacion: "https://maps.app.goo.gl/HFv7RjbetbU9fR1CA",
+        url: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6205.27475851252!2d-68.0529558!3d-38.9551118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33d462cc3157%3A0x51e8005c84277f11!2sIndependencia%20428%2C%20Q8300%20Neuqu%C3%A9n!5e0!3m2!1ses!2sar!4v1760044851950!5m2!1ses!2sar",
     },
     rktNight: {
         titulo: "RKT Night",
@@ -150,7 +149,7 @@ const eventos = {
         apertura: "19:30hs",
         cierre: "03:00hs",
         contacto: "https://instagram.com/moodliveok",
-        ubicacion: "https://maps.app.goo.gl/HFLYTMRo8y1SW6pb8",
+        url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3102.790159065663!2d-68.0583724!3d-38.951622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x960a33196ae9bf47%3A0x8935eaa087a2d345!2sMood%20Live!5e0!3m2!1ses!2sar!4v1760044539504!5m2!1ses!2sar",
     },
 };
 
