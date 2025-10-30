5d3e5aac4b156a06309a93a49ab1507b3f4244ba
jjaleniecki <jalenieckij@gmail.com>
2025-10-30 19:29:14 -0300

    Login base

--- TP2/script.js
+++ TP2/script.js
@@ -182,3 +182,13 @@ if (evento) {
     `;
     document.title = evento.titulo;
 }
+
+function toggler(e) {
+    if( e.innerHTML == 'Show' ) {
+        e.innerHTML = 'Hide'
+        document.getElementById('password').type="text";
+    } else {
+        e.innerHTML = 'Show'
+        document.getElementById('password').type="password";
+    }
+}
