3a2e502a635621d984f7a4b320985daf39445c30
mili <milagros.ruiz@est.fi.uncoma.edu.ar>
2025-12-03 23:56:29 -0300

    modularice

--- TP2/src/models/models_eventos.js
+++ TP2/src/models/models_eventos.js
@@ -15,4 +15,30 @@ function obtenerEventoJson(id){
     return eventosJson.find(evento => evento.id_evento == id );
 }
 
-module.exports = {obtenerEventoJson, obtenerEventosJson};
+function cargarEventoParticular(contenedor, evento){
+    contenedor.innerHTML = `  <h1 class = "evento-particular">
+                <p>${evento.titulo}</p>
+            </h1>
+            <div class = "eventosparticulares">
+                <div class = "col1">
+                    <img src="${evento.img}"  alt="${evento.titulo}" class="img-eventoGrande">
+                </div>
+                <div class = "col2">
+                    <div class = "row1 rect">
+                        <p> Lugar: ${evento.lugar}</a></p>
+                    </div>
+                    <div class = "row2 rect"> 
+                            <p>Apertura: ${evento.apertura} | Cierre: ${evento.cierre}</p>
+                    </div>
+                    <div class = "row3 rect"> 
+                            <a href="${evento.contacto}" target="_blank">Contacto</a>
+                    </div>
+                    <div class = "row4 ">
+                        <iframe class = "rect iframe" src="${evento.url}" width = "100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>   `
+
+}
+
+module.exports = {obtenerEventoJson, obtenerEventosJson,cargarEventoParticular};
--- TP2/src/routers/routers_eventos.js
+++ TP2/src/routers/routers_eventos.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {obtenerEventosJson, obtenerEventoJson} = require('../models/models_eventos.js');
+const {obtenerEventosJson, obtenerEventoJson,cargarEventoParticular} = require('../models/models_eventos.js');
 
 
 const fs = require("fs");
@@ -18,8 +18,16 @@ router.get("/", (req, res) => {
 
 
 
-router.get("/index.html", (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/views', 'index.html'));
+router.get("/index.html", async (req, res) => {
+    //es.sendFile(path.join(__dirname, '../../public/views', 'index.html'));
+    const eventos = obtenerEventosJson();
+    const html = await fs.promises.readFile(path.join(__dirname, '../../public/views', 'index.html'), 'utf8');
+    const { JSDOM } = require('jsdom');
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const contenedorCachengue = doc.getElementById('eventos-cachengue');
+
 });
 
 router.get("/evento.html", (req, res) => {
@@ -36,30 +44,7 @@ router.get("/eventos/:id", async (req, res) => {
   const dom = new JSDOM(html);
   const doc = dom.window.document;
 
-  doc.getElementById('infoevento').innerHTML = `    
-     <h1 class = "evento-particular">
-                <p>${evento.titulo}</p>
-            </h1>
-            <div class = "eventosparticulares">
-                <div class = "col1">
-                    <img src="${evento.img}"  alt="${evento.titulo}" class="img-eventoGrande">
-                </div>
-                <div class = "col2">
-                    <div class = "row1 rect">
-                        <p> Lugar: ${evento.lugar}</a></p>
-                    </div>
-                    <div class = "row2 rect"> 
-                            <p>Apertura: ${evento.apertura} | Cierre: ${evento.cierre}</p>
-                    </div>
-                    <div class = "row3 rect"> 
-                            <a href="${evento.contacto}" target="_blank">Contacto</a>
-                    </div>
-                    <div class = "row4 ">
-                        <iframe class = "rect iframe" src="${evento.url}" width = "100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                </div>
-            </div>   
-           `
+  cargarEventoParticular(doc.getElementById('infoevento'), evento);
 
 
   res.send(dom.serialize());
